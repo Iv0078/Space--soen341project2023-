38ef0cccd961a71b9fe2ce775a484ad14bc606fa
Iv0078 <110433667+Iv0078@users.noreply.github.com>
2023-02-18 14:24:29 -0500

    Fixed login code
    Proper working classes for User, and company accounts

--- app/signin-main.js
+++ app/signin-main.js
@@ -19,6 +19,8 @@ function clearInputError(inputElement) {
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
+    const UserForm = document.querySelector("#createUserAccount");
+    const CompanyForm = document.querySelector("#createCompanyAccount");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
@@ -26,16 +28,42 @@ document.addEventListener("DOMContentLoaded", () => {
         createAccountForm.classList.remove("form--hidden");
     });
 
+    document.querySelector(".Employe__button").addEventListener("click", e => {
+        e.preventDefault();
+        createAccountForm.classList.add("form--hidden");
+        UserForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector(".Company__button").addEventListener("click", e => {
+        e.preventDefault();
+        createAccountForm.classList.add("form--hidden");
+        CompanyForm.classList.remove("form--hidden");
+    });
+
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
+        UserForm.classList.add("form--hidden");
+        CompanyForm.classList.add("form--hidden");
     });
+
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    
+    
+   
+
 
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
-        // Perform your AJAX/Fetch login
+        // Perform Fetch login
 
         setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
@@ -52,3 +80,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+
