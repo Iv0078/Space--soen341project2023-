844f5fb082cfaed57621e6a9a467fb1554fd9be9
Adam Boucher <adampboucher@gmail.com>
2023-02-18 11:27:22 -0500

    Update connect.js

--- app/database/connect.js
+++ app/database/connect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async (databaseName) => {
    try {
-      const { connection } = await mongoose.connect(`mongodb+srv://adampboucher:SOEN341@cluster0.v5jdyed.mongodb.net/$(databaseName)?retryWrites=true&w=majority`)
+      const { connection } = await mongoose.connect(`mongodb+srv://adampboucher:SOEN341@cluster0.v5jdyed.mongodb.net/${databaseName}?retryWrites=true&w=majority`)
 
       if (connection.readystate === 1) {
          return Promise.resolve(true)
